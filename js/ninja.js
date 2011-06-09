6b884ac9710cac5564c37347a6524500a910d301
Jared Hardy <jared@jaredhardy.com>
2011-06-09 10:37:00 -0700

    Broswer fixes

--- js/ninja.js
+++ js/ninja.js
@@ -1,108 +1,118 @@
-$(function() {
-  
+
  $(window).load(function() {
-  
-  $("#intro").show();
-  $("#me").show();
-
-  setTimeout(function(){
-      if($("body").hasClass("intro")) {
-      $("body").removeClass().addClass("about");
-      }
-    }, 9750); 
-
-  function checkClass(bodyClass) {
+	
+	$("#intro").show();
+	$("#me").show();
+	
 
 
-      if(bodyClass != "skills") {
+	if($("body").hasClass("intro")) {
+		if($("html").hasClass("no-cssanimations")) {
+				setTimeout(function() {
+							
+					$("#intro").fadeOut(500);
+					$("#about").fadeIn(500);
+					$("body").removeClass().addClass("about");
+						
+				}, 4000);
+						
+		} else {
+				setTimeout(function(){	
+			
+					$("body").removeClass().addClass("about");
+				
+				}, 9750); 
+		}
+	}
+	function checkClass(bodyClass) {
 
-        $(".icons li").css({'top' : '-55px'});
+			if(bodyClass !== "skills") {
 
-      }
+				$(".icons li").css({'top' : '-55px'});
+			}
 
 
-    if(bodyClass == "skills") {
-      $icon = $(".icons li");
-       var i = 0; 
+		if(bodyClass === "skills") {
+			var $icon = $(".icons li");
+			var i = 0; 
 
-      function popIcon() {    
-       $($icon[i]).animate({'top' : '30px'}, 60).animate({'top' : '20px'}, 60, popIcon);
-       i++;
-      } 
+		  var pop = function popIcon() {	  
+			 $($icon[i]).animate({'top' : '30px'}, 60).animate({'top' : '20px'}, 60, popIcon);
+		   i++;
+			};
 
-      if(i >= $icon.length) {
-        $("#skills h3").fadeIn();
-
-      }
-      popIcon();  
-    }
+			pop();  
+		}
 
 
 
-  }  
+	}	 
 
-  var $icons = $(".icons li"),
-      $skills = $(".skill-descriptions li"),
-      $marker = $(".skill-marker"),
-      markerMoveAmount = 35,
-      markerOffset = 225,
-      iconClasses = ["punch", "kick", "closeline", "uppercut", "sweepkick", "crane", "flyingkick", "bearhug"],            
-      current;
 
-    $icons.click(function() {
-        var index = $(this).index();
-        if(current === index) {
-          return;
-        }
+		var $icons = $(".icons li"),
+				$skills = $(".skill-descriptions li"),
+				$marker = $(".skill-marker"),
+				markerOffset = 225,
+				iconClasses = ["punch", "kick", "closeline", "uppercut", "sweepkick", "crane", "flyingkick", "bearhug"],						
+				current;
 
-        if($(".skill-maker:hidden")) {
-          $marker.fadeIn(400);
-          $("#skills h3").hide();
-        }
-        current = $(this).index();
+		$icons.click(function() {
+				var index = $(this).index();
+				if(current === index) {
+					return;
+				}
 
-        var markerMoveTo = markerOffset + (($(this).outerWidth() + parseInt($(this).css('margin-right'))) * current);
+				if($(".skill-maker:hidden")) {
+					$marker.fadeIn(400);
+					$("#skills h3").hide();
+				}
+				current = $(this).index();
 
-        $("#me").removeClass().addClass(iconClasses[current]);
+				var markerMoveTo = markerOffset + (($(this).outerWidth() + parseInt($(this).css('margin-right'), 10)) * current);
 
-        $skills.fadeOut(800);
-        $($skills[current]).fadeIn(800);
-        $marker.css({'left' : markerMoveTo })
+				$("#me").removeClass().addClass(iconClasses[current]);
 
-    });
-    
-    $icons.hover(function() {
-      $(this).css({'top' : '15px'})
-    }, function() {
-      $(this).css({'top' : '20px'})
+				$skills.fadeOut(800);
+				$($skills[current]).fadeIn(800);
+				$marker.css({'left' : markerMoveTo });
 
-    });
+		});
 
-    var $dots = $(".dots li"),
-        $ems = $("#about em"),
-        dataClass = "";
+		$icons.hover(function() {
+			$(this).css({'top' : '15px'});
+		}, function() {
+			$(this).css({'top' : '20px'});
 
-      $ems.mouseover(function() {
+		});
 
-        dataClass = "." + $(this).attr("data-class");
-        $(dataClass).addClass("active-dot");
 
-      }).mouseout(function() {      
+		var $ems = $("#about em"),
+				dataClass = "";
 
-        $(dataClass).removeClass("active-dot");
 
-    });
-    
-    var $buttons = $("nav li"),
-        navClass = ["about", "skills", "contact"];
+			$ems.mouseover(function() {
 
-    $buttons.click(function() {
-      var currentClass = navClass[$(this).index()]
-      $buttons.removeClass();
+				dataClass = "." + $(this).attr("data-class");
+				$(dataClass).addClass("active-dot");
 
-      $(this).addClass("current");
-      $("body").removeClass().addClass(currentClass);
-      checkClass(currentClass);
-    });
-  }); 
+			}).mouseout(function() {			
+
+				$(dataClass).removeClass("active-dot");
+
+			});
+
+		var $buttons = $("nav li"),
+				navClass = ["about", "skills", "contact"];
+
+		$buttons.click(function() {
+			var currentClass = navClass[$(this).index()];
+			$buttons.removeClass();
+
+			$(this).addClass("current");
+			$("body").removeClass().addClass(currentClass);
+			checkClass(currentClass);
+
+
+		});
+	
 });
